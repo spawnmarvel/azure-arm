3d1f38ed3be9da9820249c9bab8cdbb249ec1fc7
spawnmarvel <espenkleivane@gmail.com>
2021-01-21 23:00:25 +0100

    readme.md

--- power_get_files_in_container.ps1
+++ power_get_files_in_container.ps1
@@ -1,7 +1,7 @@
 # Import-Module -Name Az
 # If you have a storage account, a container (blob) and a file in that blob, i.e a files called test.ps1 or what ever
 # You can access the blob and get that file and store it in home dir, ie transfer it.
-$key = Get-Content key.txt # go to the storage account\settings\access keys and cp the key 1 or key2, store it in the txt file
+$key = Get-Content key.log # go to the storage account\settings\access keys and cp the key 1 or key2, store it in the txt file
 # set the context, go to storage account and get the key
 $StorageContext = New-AzureStorageContext -StorageAccountName "booseewrqh6admd5mg" -StorageAccountKey $key
 Write-Host $StorageContext
@@ -17,5 +17,5 @@ Write-Host $blobs.Name
 $file = Get-AzureStorageBlobContent -Container $Container.Name -Context $StorageContext $blobs.Name
 
 
-# Error offline, nut not in shell in the portal
+# Error offline, but not in shell in the portal
 # https://stackoverflow.com/questions/27722205/new-azurestoragecontext-is-not-recognized/27771597
