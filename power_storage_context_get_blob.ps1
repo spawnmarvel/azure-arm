bbf9b821dbe3d0fa15a4ed19a83accc099f1c0f1
spawnmarvel <espenkleivane@gmail.com>
2021-01-21 22:26:38 +0100

    get storage content

---
+++ power_storage_context_get_blob.ps1
@@ -0,0 +1,27 @@
+
+# If you have a storage account, a container (blob) and a file in that blob, i.e a files called test.ps1 or what ever
+# You can access the blob and get that file and store it in home dir, ie transfer it.
+
+$resourceGr = "boose-rg"
+$group = Get-AzResource -ResourceGroupName $resourceGr
+# just loop over rg's
+foreach ($g in $group) {
+    Write-Host $g.Name + " " $g.Sku.Name
+    
+}
+$key = Get-Content key.txt # go to the storage account\settings\access keys and cp the key 1 or key2, store it in the txt file
+# set the context, go to storage account and get the key
+$StorageContext = New-AzureStorageContext -StorageAccountName "booseewrqh6admd5mg" -StorageAccountKey $key
+Write-Host $StorageContext
+# use the context to get the container
+$Container = Get-AzureStorageContainer -Name "boosecont" -Context $StorageContext 
+Write-Host $Container
+Write-Host $Container.Name
+# $Container  | Get-Member # just to view methods and properties
+# get the blob from the container based on contxt
+$blobs = Get-AzureStorageBlob -Container $Container.Name -Context $StorageContext
+Write-Host $blobs.Name
+# now download the folder by blobs.Name
+$file = Get-AzureStorageBlobContent -Container $Container.Name -Context $StorageContext $blobs.Name
+
+
