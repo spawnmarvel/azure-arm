35cf6f44daf402c2c944a7ad102f0185f4de2205
spawnmarvel <espenkleivane@gmail.com>
2021-01-23 23:51:48 +0100

    putty image

---
+++ Vm/simple-vm-user-pass/remove_rg.ps1
@@ -0,0 +1,17 @@
+
+$resourceGr = "testit2-rg"
+
+Get-AzResourceGroup -Name $resourceGr
+
+$str =  Get-Date
+Write-Host "List resources " $str " in " $resourceGr
+
+$group = Get-AzResource -ResourceGroupName $resourceGr
+
+foreach ($g in $group) {
+    Write-Host $g.Name + " " $g.Sku.Name
+    
+}
+
+# Delete all resources
+Get-AzResourceGroup -Name $resourceGr | Remove-AzResourceGroup -Force -AsJob
