9300caeb61570fb32d3478e879e2fe3d0ca00ea6
spawnmarvel <espenkleivane@gmail.com>
2021-01-23 19:57:03 +0100

    added power secure user

---
+++ power_user_pass_secure_string.ps1
@@ -0,0 +1,18 @@
+# Use keyvault or
+# The username and password for the VM are converted to a secure string and system object.
+$str =  Get-Date
+Write-Host "Predeploy " $str
+Write-Host "Provide a password (length > 12) for the admin user"
+$userName = "testadmin"
+$passWord = Read-Host # -AsSecureString
+if ($passWordSecure.length -le  12) {
+    Write-Host "Adding suffix to pass word, due to length must be > 12" -ForegroundColor Yellow
+    $passWord += "longpass"
+}
+Write-Host "Provided:" $passWord
+$passWordSecure = ConvertTo-SecureString $passWord -AsPlainText -Force
+Write-Host "Secure:" $passWordSecure
+$creds = New-Object System.Management.Automation.PSCredential($userName, $passWordSecure)
+
+Write-Host "Provided: " $userName ", " $passWord
+Write-Host "Secured: " $passWordSecure ", " $creds
