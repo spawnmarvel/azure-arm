df4f805129889bc6a48da9d80a4544ad7b16944a
spawnmarvel <espenkleivane@gmail.com>
2021-01-29 23:57:06 +0100

    power deploy minimum 2

--- power_deploy_minimum.ps1
+++ power_deploy_minimum.ps1
@@ -1,4 +1,8 @@
-$resourceGr = "boose-rg1"
-$templateFile = "Arm-minimum\template.parameters.json"
-$templateParameters = "Arm-minimum\template.parameters.json"
-New-AzResourceGroupDeployment -Name MinimumTemp -ResourceGroupName $resourceGr.ResourceGroupName -TemplateFile $templateFile -TemplateParameterFile $templateParameters -WhatIf
+$rgName = "testit2-rg"
+$ran = Get-Random -Maximum 100
+$deployName = "buildTestVm1" + $ran
+$templateFile = ".\template.json"
+$templateParameters = ".\template.parameters.json"
+$resourceGr = New-AzResourceGroup -Name $rgName -Location "west europe" -Force
+New-AzResourceGroupDeployment -Name $deployName -ResourceGroupName $resourceGr.ResourceGroupName `
+ -TemplateFile $templateFile -TemplateParameterFile $templateParameters -preFixAccount "account12" -whatif
